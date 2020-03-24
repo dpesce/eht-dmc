0d4ced1a1b18da07a928b459cffba201f9f83a64
dpesce <dom.pesce@gmail.com>
2020-03-24 00:57:18 -0400

    added some plotting utilities

--- examples/testscript.py
+++ examples/testscript.py
@@ -5,12 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ehtim as eh
 import dmc as dm
+import pickle
 
 #######################################################
 # loading in data
 
 # convert uvfits file to ehtim obsdata object
-obsfile = 'hops_lo_3601_M87+zbl-dtcal_selfcal.uvfits'
+obsfile = 'hops_lo_3600_M87+zbl-dtcal_selfcal.uvfits'
 obs = eh.obsdata.load_uvfits(obsfile,polrep='circ')
 
 # convert antenna file to ehtim array object
@@ -34,8 +35,8 @@ obs = obs.flag_bl(['JC','SM'])
 # fit a polarized image model
 
 # number of pixels in each dimension
-nx = 20
-ny = 20
+nx = 6
+ny = 6
 
 # axis ranges in each dimension
 xmin = -30.0
@@ -43,7 +44,35 @@ xmax = 30.0
 ymin = -30.0
 ymax = 30.0
 
-output = dm.models.polim(obs,nx,ny,xmin,xmax,ymin,ymax)
+# set number of burn-in, tuning, and sampling steps
+ntuning = 2000
+ntrials = 10000
+
+# perform the model-fitting (note: takes a long time!)
+modelinfo = dm.models.polim(obs,nx,ny,xmin,xmax,ymin,ymax,ntuning=ntuning,ntrials=ntrials)
+
+# saving the trace file
+pickle.dump(modelinfo,open('modelinfo.p','wb'),protocol=pickle.HIGHEST_PROTOCOL)
+
+#######################################################
+# create some summary plots
+
+# modelinfo = pickle.load(open('modelinfo.p','rb'))
+
+# save a set of trace plots
+dm.plotting.plot_trace(modelinfo,var_names=['f','I','Q','U','V','right_gain_amps','left_gain_amps','right_gain_phases','left_gain_phases','right_Dterm_reals','left_Dterm_reals','right_Dterm_imags','left_Dterm_imags'])
+plt.savefig('traceplots.png',dpi=300)
+plt.close()
+
+# save Stokes I plots
+dm.plotting.plot_image(modelinfo,'I','mean')
+plt.savefig('StokesI_mean.png',dpi=300)
+plt.close()
+
+
+
+
+
 
 
 
