bb2f1e9e531702e9d0132e3266ee2cc633eddca6
dpesce <dom.pesce@gmail.com>
2020-09-19 00:29:28 -0400

    temporary update to make_image for checking the new FOV convention

--- eht_dmc/model_utils.py
+++ eht_dmc/model_utils.py
@@ -571,6 +571,360 @@ def make_image(modelinfo,moment='mean',nx=100,ny=100,fov_scale=1.5,burnin=0,nsam
 
     return im
 
+def make_image2(modelinfo,moment='mean',nx=100,ny=100,fov_scale=1.5,burnin=0,nsamps=None,mean_image=None):
+    """ Make eht-imaging image object
+
+       Args:
+           modelinfo (dict): dmc modelinfo dictionary
+           moment (str): the type of posterior moment to save; choices are mean, std
+           nx (int): number of image pixels in the x-direction
+           ny (int): number of image pixels in the y-direction
+           fov_scale (float): factor by which to increase the model FOV when making the image
+           burnin (int): length of burn-in
+           nsamps (int): number of chain samples to use when making the image
+           mean_image (image): ehtim image object containing the mean image, if precomputed
+           
+       Returns:
+           im: ehtim image object
+    
+    """
+    if modelinfo['modeltype'] not in ['image','polimage']:
+        raise Exception('modeltype is not image or polimage!')
+    if moment not in ['mean','std']:
+        raise Exception('moment ' + moment + ' not recognized!')
+    if nx != ny:
+        raise Exception('nx is not equal to ny!  eht-imaging does not support rectangular images.')
+
+    ###################################################
+    # organizing image information
+
+    nx_input = modelinfo['nx']
+    ny_input = modelinfo['ny']
+    FOVx = modelinfo['FOVx']
+    FOVy = modelinfo['FOVy']
+    x0 = modelinfo['x0']
+    y0 = modelinfo['y0']
+
+    psize_x = FOVx / float(nx_input)
+    psize_y = FOVy / float(ny_input)
+    xmin = x0 - ((FOVx-psize_x)/2.0)
+    xmax = x0 + ((FOVx-psize_x)/2.0)
+    ymin = y0 - ((FOVy-psize_y)/2.0)
+    ymax = y0 + ((FOVy-psize_y)/2.0)
+
+    smooth = modelinfo['smooth']
+    fit_smooth = modelinfo['fit_smooth']
+    
+    fullwidth_x = xmax - xmin
+    pix_size_x = fullwidth_x / (nx-1)
+    fov_x = nx*pix_size_x
+
+    fullwidth_y = ymax - ymin
+    pix_size_y = fullwidth_y / (ny-1)
+    fov_y = ny*pix_size_y
+
+    if ((smooth != None) & (fit_smooth == False)):
+        sigma = smooth / (2.0*np.sqrt(2.0*np.log(2.0)))
+    
+    # one-dimensional pixel vectors for the model
+    x_1d = np.linspace(xmin,xmax,nx_input)
+    y_1d = np.linspace(ymin,ymax,ny_input)
+    
+    # one-dimensional pixel vectors for the image
+    xim_1d = np.linspace(fov_scale*xmin,fov_scale*xmax,nx)
+    yim_1d = np.linspace(fov_scale*ymin,fov_scale*ymax,ny)
+
+    # two-dimensional pixel vectors for the image
+    xim_2d, yim_2d = np.meshgrid(xim_1d,yim_1d,indexing='ij')
+    x = xim_2d.ravel()
+    y = yim_2d.ravel()
+
+    # make edge arrays
+    xspacing = np.mean(xim_1d[1:]-xim_1d[0:-1])
+    x_edges_1d = np.append(xim_1d,xim_1d[-1]+xspacing) - (xspacing/2.0)
+    yspacing = np.mean(yim_1d[1:]-yim_1d[0:-1])
+    y_edges_1d = np.append(yim_1d,yim_1d[-1]+yspacing) - (yspacing/2.0)
+    x_edges, y_edges = np.meshgrid(x_edges_1d,y_edges_1d,indexing='ij')
+
+    # pixel size for the image
+    psize_x_im = fov_scale*(xmax-xmin)/(nx-1)
+    psize_y_im = fov_scale*(ymax-ymin)/(ny-1)
+
+    ###################################################
+    # organize chain info and compute moment
+
+    trace = modelinfo['trace']
+
+    # remove burnin
+    I = trace['I'][burnin:]
+    if (modelinfo['modeltype'] == 'polimage'):
+        Q = trace['Q'][burnin:]
+        U = trace['U'][burnin:]
+        V = trace['V'][burnin:]
+
+    # remove divergences
+    div_mask = np.invert(trace[burnin:].diverging)
+    I = I[div_mask]
+    if (modelinfo['modeltype'] == 'polimage'):
+        Q = Q[div_mask]
+        U = U[div_mask]
+        V = V[div_mask]
+
+    # initialize image vector
+    Ivec = np.zeros(nx*ny)
+    if (modelinfo['modeltype'] == 'polimage'):
+        Qvec = np.zeros(nx*ny)
+        Uvec = np.zeros(nx*ny)
+        Vvec = np.zeros(nx*ny)
+
+    # loop over samples
+    if (modelinfo['modeltype'] == 'image'):
+
+        # check if the mean image has already been provided
+        if (mean_image is None):
+
+            if (nsamps is not None):
+                print('Computing mean using '+str(nsamps)+' samples from the chain...')
+                for i in tqdm(range(nsamps)):
+
+                    index = np.random.randint(len(I))
+                    Ivec_here = I[index].reshape((nx_input,ny_input))
+
+                    # loop over modeled pixels
+                    for ix in range(len(x_1d)):
+                        for iy in range(len(y_1d)):
+                            Ivec += (Ivec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+
+                # get average
+                Ivec /= float(nsamps)
+
+            # if number of samples isn't specified, loop over the entire chain
+            else:
+                print('Warning: nsamps is not specified; computing mean over the entire chain!')
+                for i in tqdm(range(len(I))):
+
+                    Ivec_here = I[i].reshape((nx_input,ny_input))
+
+                    # loop over modeled pixels
+                    for ix in range(len(x_1d)):
+                        for iy in range(len(y_1d)):
+                            Ivec += (Ivec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+
+                # get average
+                Ivec /= float(len(I))
+
+        # if mean image has been provided, use it
+        else:
+            Ivec = mean_image.ivec.reshape((nx,ny)).T[::-1,::-1].ravel()
+
+        # compute standard deviation
+        if (moment == 'std'):
+            Ivec_avg = np.copy(Ivec)
+
+            # initialize image vector
+            Ivec = np.zeros(nx*ny)
+
+            # loop over samples
+            if (nsamps is not None):
+                print('Computing standard deviation using '+str(nsamps)+' samples from the chain...')
+                for i in tqdm(range(nsamps)):
+
+                    index = np.random.randint(len(I))
+                    Ivec_here = I[index].reshape((nx_input,ny_input))
+
+                    # loop over modeled pixels
+                    imhere = np.zeros(nx*ny)
+                    for ix in range(len(x_1d)):
+                        for iy in range(len(y_1d)):
+                            imhere += (Ivec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                    Ivec += (imhere - Ivec_avg)**2.0
+
+                # get standard deviation
+                Ivec = np.sqrt(Ivec/float(nsamps))
+
+            # if number of samples isn't specified, loop over the entire chain
+            else:
+                print('Warning: nsamps is not specified; computing standard deviation over the entire chain!')
+                for i in tqdm(range(len(I))):
+
+                    Ivec_here = I[i].reshape((nx_input,ny_input))
+
+                    # loop over modeled pixels
+                    imhere = np.zeros(nx*ny)
+                    for ix in range(len(x_1d)):
+                        for iy in range(len(y_1d)):
+                            imhere += (Ivec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                    Ivec += (imhere - Ivec_avg)**2.0
+
+                # get average
+                Ivec = np.sqrt(Ivec/float(len(I)))
+
+        # reshape array
+        Ivec = Ivec.reshape((nx,ny))
+
+    # loop over samples
+    elif (modelinfo['modeltype'] == 'polimage'):
+
+        # check if the mean image has already been provided
+        if (mean_image is None):
+
+            if (nsamps is not None):
+                print('Computing mean using '+str(nsamps)+' samples from the chain...')
+                for i in tqdm(range(nsamps)):
+
+                    index = np.random.randint(len(I))
+                    Ivec_here = I[index].reshape((nx_input,ny_input))
+                    Qvec_here = Q[index].reshape((nx_input,ny_input))
+                    Uvec_here = U[index].reshape((nx_input,ny_input))
+                    Vvec_here = V[index].reshape((nx_input,ny_input))
+
+                    # loop over modeled pixels
+                    for ix in range(len(x_1d)):
+                        for iy in range(len(y_1d)):
+                            Ivec += (Ivec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Qvec += (Qvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Uvec += (Uvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Vvec += (Vvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+
+                # get average
+                Ivec /= float(nsamps)
+                Qvec /= float(nsamps)
+                Uvec /= float(nsamps)
+                Vvec /= float(nsamps)
+
+            # if number of samples isn't specified, loop over the entire chain
+            else:
+                print('Warning: nsamps is not specified; computing mean over the entire chain!')
+                for i in tqdm(range(len(I))):
+
+                    Ivec_here = I[i].reshape((nx_input,ny_input))
+                    Qvec_here = Q[i].reshape((nx_input,ny_input))
+                    Uvec_here = U[i].reshape((nx_input,ny_input))
+                    Vvec_here = V[i].reshape((nx_input,ny_input))
+
+                    # loop over modeled pixels
+                    for ix in range(len(x_1d)):
+                        for iy in range(len(y_1d)):
+                            Ivec += (Ivec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Qvec += (Qvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Uvec += (Uvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Vvec += (Vvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+
+                # get average
+                Ivec /= float(len(I))
+                Qvec /= float(len(Q))
+                Uvec /= float(len(U))
+                Vvec /= float(len(V))
+
+        # if mean image has been provided, use it
+        else:
+            Ivec = mean_image.ivec.reshape((nx,ny)).T[::-1,::-1].ravel()
+            Qvec = mean_image.qvec.reshape((nx,ny)).T[::-1,::-1].ravel()
+            Uvec = mean_image.uvec.reshape((nx,ny)).T[::-1,::-1].ravel()
+            Vvec = mean_image.vvec.reshape((nx,ny)).T[::-1,::-1].ravel()
+
+        # compute standard deviation
+        if (moment == 'std'):
+            Ivec_avg = np.copy(Ivec)
+            Qvec_avg = np.copy(Qvec)
+            Uvec_avg = np.copy(Uvec)
+            Vvec_avg = np.copy(Vvec)
+
+            # initialize image vector
+            Ivec = np.zeros(nx*ny)
+            Qvec = np.zeros(nx*ny)
+            Uvec = np.zeros(nx*ny)
+            Vvec = np.zeros(nx*ny)
+
+            # loop over samples
+            if (nsamps is not None):
+                print('Computing standard deviation using '+str(nsamps)+' samples from the chain...')
+                for i in tqdm(range(nsamps)):
+
+                    index = np.random.randint(len(I))
+                    Ivec_here = I[index].reshape((nx_input,ny_input))
+                    Qvec_here = Q[index].reshape((nx_input,ny_input))
+                    Uvec_here = U[index].reshape((nx_input,ny_input))
+                    Vvec_here = V[index].reshape((nx_input,ny_input))
+
+                    # loop over modeled pixels
+                    Ihere = np.zeros(nx*ny)
+                    Qhere = np.zeros(nx*ny)
+                    Uhere = np.zeros(nx*ny)
+                    Vhere = np.zeros(nx*ny)
+                    for ix in range(len(x_1d)):
+                        for iy in range(len(y_1d)):
+                            Ihere += (Ivec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Qhere += (Qvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Uhere += (Uvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Vhere += (Vvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+
+                    Ivec += (Ihere - Ivec_avg)**2.0
+                    Qvec += (Qhere - Qvec_avg)**2.0
+                    Uvec += (Uhere - Uvec_avg)**2.0
+                    Vvec += (Vhere - Vvec_avg)**2.0
+
+                # get standard deviation
+                Ivec = np.sqrt(Ivec/float(nsamps))
+                Qvec = np.sqrt(Qvec/float(nsamps))
+                Uvec = np.sqrt(Uvec/float(nsamps))
+                Vvec = np.sqrt(Vvec/float(nsamps))
+
+            # if number of samples isn't specified, loop over the entire chain
+            else:
+                print('Warning: nsamps is not specified; computing standard deviation over the entire chain!')
+                for i in tqdm(range(len(I))):
+
+                    Ivec_here = I[i].reshape((nx_input,ny_input))
+                    Qvec_here = Q[i].reshape((nx_input,ny_input))
+                    Uvec_here = U[i].reshape((nx_input,ny_input))
+                    Vvec_here = V[i].reshape((nx_input,ny_input))
+
+                    # loop over modeled pixels
+                    Ihere = np.zeros(nx*ny)
+                    Qhere = np.zeros(nx*ny)
+                    Uhere = np.zeros(nx*ny)
+                    Vhere = np.zeros(nx*ny)
+                    for ix in range(len(x_1d)):
+                        for iy in range(len(y_1d)):
+                            Ihere += (Ivec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Qhere += (Qvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Uhere += (Uvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+                            Vhere += (Vvec_here[ix,iy]/(2.0*np.pi*(sigma/psize_x_im)*(sigma/psize_y_im)))*np.exp(-((((x-x_1d[ix])**2.0) + ((y-y_1d[iy])**2.0)) / (2.0*(sigma**2.0))))
+
+                    Ivec += (Ihere - Ivec_avg)**2.0
+                    Qvec += (Qhere - Qvec_avg)**2.0
+                    Uvec += (Uhere - Uvec_avg)**2.0
+                    Vvec += (Vhere - Vvec_avg)**2.0
+
+                # get average
+                Ivec = np.sqrt(Ivec/float(len(I)))
+                Qvec = np.sqrt(Qvec/float(len(Q)))
+                Uvec = np.sqrt(Uvec/float(len(U)))
+                Vvec = np.sqrt(Vvec/float(len(V)))
+
+        # reshape array
+        Ivec = Ivec.reshape((nx,ny))
+        Qvec = Qvec.reshape((nx,ny))
+        Uvec = Uvec.reshape((nx,ny))
+        Vvec = Vvec.reshape((nx,ny))
+
+    ###################################################
+    # create eht-imaging image object
+
+    # transfer observation data
+    obs = modelinfo['obs']
+    im = eh.image.Image(Ivec, psize=xspacing*eh.RADPERUAS, ra=obs.ra, dec=obs.dec, polrep='stokes', rf=obs.rf, source=obs.source, mjd=obs.mjd, time=np.mean(obs.data['time']))
+
+    # populate image
+    im.ivec = Ivec.T[::-1,::-1].ravel()
+    if (modelinfo['modeltype'] == 'polimage'):
+        im.qvec = Qvec.T[::-1,::-1].ravel()
+        im.uvec = Uvec.T[::-1,::-1].ravel()
+        im.vvec = Vvec.T[::-1,::-1].ravel()
+
+    return im
+
 def save_fits(modelinfo,moment,outfile,burnin=0):
     """ Save eht-imaging compatible fits file
 
